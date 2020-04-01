a5b4e233ed5dd25b3c25b01dd00da1ed1d074cb5
malika-k <mkrkodial@gmail.com>
2020-03-31 23:31:21 -0400

    Created api call functions - nested for 2 calls

--- React/myweb-v1/src/containers/App.js
+++ React/myweb-v1/src/containers/App.js
@@ -19,6 +19,108 @@ class App extends Component {
       error: false
     }
 
+    // Update state with current search bar input
+  searchBarHandler = (e) => {
+      this.setState({
+        searchBarInput: e.target.value
+      })
+    }
+
+    // Reset state after clicking on Logo or Try Again button
+  tryAgainHandler = () => {
+      this.setState({
+        searchBarInput: '',
+        artistDisplay: {
+          name: '',
+          events: [],
+          fb: '',
+          imdb: '',
+          picture: ''
+        },
+        error: false
+      })
+    }
+
+
+  fetchAPI = (api) => {
+    return fetch(api).then(res => res.json());
+  }
+
+  fetchArtist = () => {
+
+    const artist = this.state.searchBarInput;
+
+    const API_KEY = process.env.REACT_APP_API_KEY;
+    const API_URL = `https://rest.bandsintown.com/artists/${artist}`;
+
+    const ARTIST_URL = API_URL + `?app_id=${API_KEY}`;
+    const EVENT_URL = API_URL + `/events?app_id=${API_KEY}&date=upcoming`;
+
+
+    this.setState({
+      artistDisplay: {
+        name: '',
+        events: [],
+        fb: '',
+        imdb: '',
+        picture: ''
+      },
+      loading: true,
+      error: false
+    }, () => {
+      // Executed as callback function
+      fetch(ARTIST_URL)
+        .then(res => res.json())
+          .then(artistdata => {
+          this.fetchAPI(EVENT_URL)
+            .then(eventdata => {
+              //filter both event and artist data
+              let output = [artistdata, eventdata];
+              console.log(output);
+
+              // If city exists, update weather details
+              if(artistdata.cod === 200) {
+                this.setState({
+                  artistDisplay: {
+                    name: '',
+                    events: [],
+                    fb: '',
+                    imdb: '',
+                    picture: ''
+                  },
+                  loading: false
+                });
+              } else {
+                // If artist doesn't exist, throw error
+                throw artistdata.cod
+              }
+            })
+            .catch(err => {
+              console.log(err);
+              this.setState({
+                loading: false,
+                error: true
+              });
+            });
+        });
+
+
+      fetch(EVENT_URL)
+        .then(res => res.json())
+        .then(data => {
+
+        })
+        .catch(err => {
+          console.log(err);
+          this.setState({
+            loading: false,
+            error: true
+          });
+        });
+    });
+  }
+
+
   render() {
     return (
       <div className={classes.AppWrapper}>
